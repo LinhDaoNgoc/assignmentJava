1106d1db4e3fb63c1878d1799fb7cfce91e43ff7
LinhDaoNgoc <daongoclinh22@gmail.com>
2022-03-30 08:04:22 +0700

    update

--- src/components/header.js
+++ src/components/header.js
@@ -67,9 +67,8 @@ const Header = {
                 class="in-up--sub absolute top-[70px] bg-white shadow-xl z-50 p-3 rounded-lg invisible ease-linear duration-300 w-60 right-[-96px] group-hover:visible before:absolute before:-top-2 before:left-10 before:w-5 before:h-5 before:bg-white before:rounded before:rotate-45 before:z-10 before:shadow-xl"
               >
 
-              ${
-                localStorage.getItem("user")
-                  ? /* HTML */ `
+              ${localStorage.getItem("user")
+        ? /* HTML */ `
                       <div class="flex items-center pb-3 w-full">
                         <div class="ml-4">
                           <div class="text-sm text-gray-500">Xin chào !</div>
@@ -95,11 +94,11 @@ const Header = {
                         </li>
                       </ul>
                     `
-                  : `<ul>
+        : `<ul>
                                 <li class="rounded-lg text-black hover:bg-[#ffbe33] hover:text-white"><a class="inline-block p-2" href="/signin">Đăng nhập</a></li>
                                 <li class="rounded-lg text-black hover:bg-[#ffbe33] hover:text-white"><a class="inline-block p-2" href="/signup">Đăng ký</a></li>
                                </ul>`
-              }
+      }
               </div>
 
 
